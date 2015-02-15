8796405afb34c8e4b2a91ae5c39ef04cfe067cb6
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2015-02-15 15:17:55 +0300

    Revert "add gems versions"
    This reverts commit 75fd1919095238ec1cbcc801307d2e53206c9ce3.

--- mongo.rb
+++ mongo.rb
@@ -29,9 +29,9 @@ gem 'compass-rails'
 
 # Frontend
 gem 'slim-rails'
+gem 'active_link_to'
 gem 'evil-blocks-rails'
 gem 'normalize-rails'
-gem 'svg_rails', github: 'mustangostang/svg_rails'
 
 # Business logic
 gem 'therubyracer'
@@ -46,7 +46,6 @@ gem 'mongoid_globalize'
 gem 'bson_ext'
 gem 'fog'
 gem 'devise'
-gem 'devise-russian'
 
 # Activeadmin
 gem 'activeadmin', github: 'gregbell/active_admin'
